77fcb26e16cbe7c489ff994a5d94030620cd534e
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-29 14:13:39 -0600

    adds a bookings array that stores bookings' objects

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const bookings = [];
+
 const createBooking = function (flightNum, numPassengers, price) {
   const booking = {
     flightNum,
     numPassengers,
     price,
   };
+
+  bookings.push(booking);
   console.log(booking);
 };
 
 createBooking(23, 45, 300);
+console.log(bookings);
