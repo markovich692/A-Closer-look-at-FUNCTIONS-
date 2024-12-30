94fa72d0e7df58ff4c91dc1a71a90c3d3d744efe
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-30 11:31:21 -0600

    writes a function that takes a string and removes all spaces in between letters

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  bookings.push(booking);
-  console.log(booking);
+//   bookings.push(booking);
+//   console.log(booking);
+// };
+
+// createBooking('AH666', 45, undefined);
+// createBooking('AH666', 5);
+// console.log(bookings);
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'AH666';
+//   passenger.name = 'Mr ' + passenger.name;
+//   // passenger.passport = 12345;
+
+//   if (passenger.passport === 24739479284) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passsport!');
+//   }
+//   console.log(flightNum);
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
-
-createBooking(23, 45, 300);
-createBooking(23, 34, 300);
-createBooking(26, 34, 300);
-createBooking(26);
-console.log(bookings);
