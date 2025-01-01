35c2201b483581ddd872de0d0cbf227a9943cfd1
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-01 16:50:08 -0600

    creates the poll object

--- script.js
+++ script.js
@@ -174,3 +174,9 @@
 // 5. Bonus:Usethe'displayResults'methodtodisplaythe2arraysinthetest data. Use both the 'array' and
 // the 'string' option. Do not put the arrays in the poll object! So what should the this keyword
 // look like in this situation?
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
+};
