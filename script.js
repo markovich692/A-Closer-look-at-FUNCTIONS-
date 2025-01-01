cf4df951ffe70223a53048195e80404e42ed4e72
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-01 14:57:50 -0600

    writes a function inside another; the grid technique to achieve the samne as what was done with the .bind() method

--- script.js
+++ script.js
@@ -86,27 +86,27 @@
 // const salute = greet('hi');
 // salute('Albert');
 
-const lufthansa = {
-  airline: 'lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-};
+// const lufthansa = {
+//   airline: 'lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+// };
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const book = function (flightNum, name) {
-  console.log(
-    `${name} booked a seat on ${this.airline}, flight ${this.iataCode}${flightNum} `
-  );
-  this.bookings.push({
-    flight: `${this.iataCode}${flightNum}`,
-    name,
-  });
-};
+// const book = function (flightNum, name) {
+//   console.log(
+//     `${name} booked a seat on ${this.airline}, flight ${this.iataCode}${flightNum} `
+//   );
+//   this.bookings.push({
+//     flight: `${this.iataCode}${flightNum}`,
+//     name,
+//   });
+// };
 
 // book.call(lufthansa, 345335, 'James');
 // book.call(lufthansa, 346666, 'Bob');
@@ -118,17 +118,33 @@ const book = function (flightNum, name) {
 
 // const bookEW23 = book.bind(lufthansa, 23);
 
-lufthansa.planes = 300;
+// lufthansa.planes = 300;
 
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
 
 // document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 // console.log(lufthansa);
+
+// const addTax = (rate, value) => value + value * rate;
+
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+
+// console.log(addVAT(23));
+
+const addTax = function (rate) {
+  return function addVAT(value) {
+    console.log(value + value * rate);
+  };
+};
+
+addTax(0.23)(23);
