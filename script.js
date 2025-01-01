5c45991371162ce0cae6a2872cb67c98dc0e3f3b
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-01 17:33:50 -0600

    displays a prompt window for the user to input the number of the selected option

--- script.js
+++ script.js
@@ -178,5 +178,14 @@
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = prompt(`${this.question}
+      ${this.options[0]}
+      ${this.options[1]}
+      ${this.options[2]}
+      ${this.options[3]}`);
+  },
 };
+
+poll.registerNewAnswer();
