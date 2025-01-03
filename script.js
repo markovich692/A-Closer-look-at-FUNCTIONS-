63fa4932b2a571679158d1e37f9f7df8914559c4
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2025-01-03 13:50:58 -0600

    starts coding challenge 2

--- script.js
+++ script.js
@@ -285,25 +285,49 @@
 // booker();
 // booker();
 
-let f;
+//Example 1
+// let f;
 
-const g = function () {
-  const a = 23;
+// const g = function () {
+//   const a = 23;
 
-  f = function () {
-    console.log(a * 2);
-  };
-};
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
 
-const h = function () {
-  const b = 777;
-  f = function () {
-    console.log(b * 2);
-  };
-};
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
 
-g();
-f();
+// g();
+// f();
 
-h();
-f();
+// h();
+// f();
+
+//Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// boardPassengers.bind(null, 333, 1)();
+
+//Challenge #2
+// This is more of a thinking challenge than a coding challenge 🤓 Your tasks:
+// 1. Take the IIFE below and at the end of the function,attach an event listener
+// that changes the color of the selected h1 element ('header') to blue,
+// each time the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked!Take
+// all the time you need. Think about when exactly the callback function is executed,
+//  and what that means for the variables involved in this example.
